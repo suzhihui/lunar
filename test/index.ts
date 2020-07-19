21737df3308f463cde22e7f5c4b00285142a7b1c
laosu <kaimilu@outlook.com>
2020-07-20 04:20:25 +0800

    fix bug

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@
 // const Calendar=require('../src/calendar')
 import Calendar from '../src/calendar'
 let calendar = new Calendar({})
-console.log(calendar.getLunarCalendar(2020,11,44))
+console.log(calendar.getLunarCalendar(2020,6,21))
 var sObj = new Date('2020-9-1 00:00:01')
 var fisrtDay:number = sObj.getDay()
 console.log(sObj, '----', fisrtDay);
